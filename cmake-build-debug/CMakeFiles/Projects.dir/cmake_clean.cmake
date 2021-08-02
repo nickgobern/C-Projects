31e68e48ac6695258c8c07a7482c54013ecf3906
DrthMrVdr <nickgobern@hotmail.com>
2021-08-02 14:31:44 -0400

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Projects.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Projects.dir/library.c.o"
+  "libProjects.a"
+  "libProjects.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Projects.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ library.h
@@ -0,0 +1,6 @@
+#ifndef PROJECTS_LIBRARY_H
+#define PROJECTS_LIBRARY_H
+
+void hello(void);
+
+#endif //PROJECTS_LIBRARY_H
